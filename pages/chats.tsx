4f1e3c8fc9f84905b75cfe3c2edafb9bdd27e957
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2022-07-27 22:34:32 +0500

    Updated

--- pages/chats.tsx
+++ pages/chats.tsx
@@ -1,4 +1,15 @@
-import React from "react";
+import React, {useState, useContext, useEffect} from "react";
+import {globalContext} from '../context/index';
+import {useRouter} from "next/router";
+import dynamic from "next/dynamic";
+
+// const ChatEngine = dynamic(()=>
+//     import("react-chat-engine").then((module) => module.ChatEngine)
+// )
+
+// const MessageFormSocial = dynamic(()=>
+//     import("react-chat-engine").then((module) => module.ChatEngine)
+// )
 
 export default function Chats() {
   return <div className="background">chats</div>;
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,13 +9,25 @@ const Auth = () => {
     setUsername,
     secret,
     setSecret} = useContext(globalContext);
+  const router = useRouter();
+  // 
+  function onSubmit(e: any){
+    e.preventDefault();
+    if(username.length == 0 || secret.length == 0) return;
+    //
+    router.push('/chats');
+  }
   //
   return <div className='background'>
     <div className='auth-container'>
-      <form className="auth-form" onSubmit={e => e.preventDefault()}>
+      <form className="auth-form" onSubmit={onSubmit}>
         <div className="auth-title">Chat</div>
         <div className="input-container">
-          <input placeholder="Email" className='text-input' onChange={e => e.preventDefault()} />
+          <input placeholder="Email" className='text-input' onChange={e => setUsername(e.target.value)} />
+          <input placeholder="Password" className='text-input' onChange={e => setSecret(e.target.value)} />
+          <button type="submit" className="submit-button">
+            Login / Sign Up
+          </button>
         </div>
       </form>
     </div>
