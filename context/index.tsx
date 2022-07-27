f0151f85b9d45b3407901a9b7dac6d461b05332b
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2022-07-27 21:48:38 +0500

    Created context type

--- context/index.tsx
+++ context/index.tsx
@@ -3,7 +3,14 @@ import React, {createContext, useState} from 'react';
 
 const initialState = {};
 
-export const globalContext = createContext(initialState);
+interface chatContextType {
+    username: any,
+    setUsername: any,
+    secret: any,
+    setSecret: any
+}
+
+export const globalContext = createContext<chatContextType>({} as chatContextType);
 
 export const ContextProvider = (props:any) => {
     const [username, setUsername] = useState();
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,24 @@ import type { NextPage } from 'next'
 import React, {useContext} from 'react';
 import {globalContext} from '../context/index';
 import {useRouter} from 'next/router';
+import axios from 'axios';
 
 const Auth = () => {
-  return <div className='background'>auth</div>
+  const { username,
+    setUsername,
+    secret,
+    setSecret} = useContext(globalContext);
+  //
+  return <div className='background'>
+    <div className='auth-container'>
+      <form className="auth-form" onSubmit={e => e.preventDefault()}>
+        <div className="auth-title">Chat</div>
+        <div className="input-container">
+          <input placeholder="Email" className='text-input' onChange={e => e.preventDefault()} />
+        </div>
+      </form>
+    </div>
+  </div>
 }
 
 export default Auth
